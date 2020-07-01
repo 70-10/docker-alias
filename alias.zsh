777a004e93841f2c831be4dfd3f6dd21be7ecf88
70_10 <7010.dev@gmail.com>
2020-07-01 10:13:56 +0900

    Add openapi-generator-cli

--- alias.zsh
+++ alias.zsh
@@ -8,3 +8,4 @@ alias d-rm-containers='docker-rm-containers'
 # application command
 alias plantuml-server="docker run -d -p 8080:8080 plantuml/plantuml-server"
 alias aws="docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli"
+alias openapi-generator="docker run -v ${PWD}:/local -w /local  openapitools/openapi-generator-cli"
