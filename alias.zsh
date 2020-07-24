f1a4f12edc5acb168f1b9cb059b8df00e690a699
70_10 <7010.dev@gmail.com>
2020-07-24 09:15:28 +0900

    Change quote

--- alias.zsh
+++ alias.zsh
@@ -6,8 +6,8 @@ alias docker-rm-containers='docker ps -aqf status=exited | xargs docker rm -v'
 alias d-rm-containers='docker-rm-containers'
 
 # application command
-alias plantuml-server="docker run -d -p 8080:8080 plantuml/plantuml-server"
-alias aws="docker run --rm -it -v ~/.aws:/root/.aws -v ${PWD}:/aws amazon/aws-cli"
-alias openapi-generator="docker run -it --rm -v ${PWD}:/local -w /local openapitools/openapi-generator-cli"
-alias radigo="docker run -it --rm -v ${PWD}:/output yyoshiki41/radigo"
-alias kindlegen="docker run -it --rm -v ${PWD}:/kindle 7010/kindlegen"
+alias plantuml-server='docker run -d -p 8080:8080 plantuml/plantuml-server'
+alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v ${PWD}:/aws amazon/aws-cli'
+alias openapi-generator='docker run -it --rm -v ${PWD}:/local -w /local openapitools/openapi-generator-cli'
+alias radigo='docker run -it --rm -v ${PWD}:/output yyoshiki41/radigo'
+alias kindlegen='docker run -it --rm -v ${PWD}:/kindle 7010/kindlegen'
