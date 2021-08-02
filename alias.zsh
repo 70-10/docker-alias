564c7298b190fd3342d5d50eaa29571fee6ebcca
70_10 <7010.dev@gmail.com>
2021-08-02 10:54:16 +0900

    Update aliases

--- alias.zsh
+++ alias.zsh
@@ -1,9 +1,5 @@
 alias d='docker'
-alias d-compose='docker-compose'
-alias docker-rmi-nones='docker images -qf dangling=true | xargs docker rmi'
-alias d-rmi-nones='docker-rmi-nones'
-alias docker-rm-containers='docker ps -aqf status=exited | xargs docker rm -v'
-alias d-rm-containers='docker-rm-containers'
+alias d-compose='docker compose'
 
 # application command
 alias plantuml-server='docker run -d --name plantuml-server -p 8080:8080 -e PLANTUML_LIMIT_SIZE=160000 plantuml/plantuml-server'
